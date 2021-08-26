8c8c23329b9a3544daea603c3ea1bc202280214f
Sean Conway <81600120+seanway28@users.noreply.github.com>
2021-08-26 07:19:47 -0400

    indexjs

---
+++ index.js
@@ -0,0 +1,7 @@
+// Include packages needed for this application
+const inquirer = require ('inquirer');
+const fs = require ('fs');
+const createMD = require ('./utils/generateMarkdown');
+
+// Create an array of questions for the user input
+const
